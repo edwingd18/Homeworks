ec2734bb8625f17973ff10abc0898b93456d54ea
edwingd18 <124949020+edwingd18@users.noreply.github.com>
2024-03-06 10:17:54 -0500

    Delete Funtions.js

--- Funtions.js
+++
@@ -1,7 +0,0 @@
-const isEvenRegular = function(number) {
-    return number % 2 == 0 ? 'Es par' : 'No es par' 
-}
-
-const isEvenArrow = (number) => {
-    return number % 2 == 0 ? 'Es par' : 'No es par' 
-}
