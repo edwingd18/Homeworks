fe6ea962c87487203e75576fd8d66047476f3749
edwingd18 <124949020+edwingd18@users.noreply.github.com>
2024-05-06 00:33:21 -0500

    use memo

--- memo/src/main.jsx
+++ memo/src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { TodoApp } from "./App.jsx";
+import Father from "./components/Father";
 import "./index.css";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <TodoApp />
+    <Father></Father>
   </React.StrictMode>
 );
